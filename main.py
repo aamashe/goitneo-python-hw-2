440928cf1d212c48946a10de8b3dd0dfefabd08b
aamashe <141754286+aamashe@users.noreply.github.com>
2023-10-28 17:12:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+
+def read_employees_from_file(users):
+    today = datetime.today().date()
+    birthdays = {}
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()
+        birthday_this_year = birthday.replace(year=today.year)
+        if birthday_this_year < today:
+            birthday_this_year = birthday.replace(year=today.year + 1)
+        if (birthday_this_year - today).days < 7:
+            day_of_week = GetNameOfDay(birthday_this_year)
+            if day_of_week in birthdays:
+                birthdays[day_of_week].append(name)
+            else:
+                birthdays[day_of_week] = [name]
+    result = ""
+    for day, names in birthdays.items():
+        result += f"{day} {', '.join(names)}\n"
+
+    return result
+
+
+def GetNameOfDay(date):
+    switcher = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday"
+    }
+    return switcher.get(date.weekday(), "Invalid day")
+
+
+print(read_employees_from_file([
+    {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)},
+    {"name": "Kalle", "birthday": datetime(1992, 10, 28)},
+    {"name": "Steve Jobs", "birthday": datetime(1955, 10, 28)},
+    {"name": "Anelia", "birthday": datetime(1996, 10, 29)}
+]))
